48c7a0efc67c83e277678c99aa92019bfd3fe6a9
chanjo77 <jocksgml1537@gmail.com>
2022-07-19 10:54:49 +0900

    slicing

---
+++ Slicing.py
@@ -0,0 +1,29 @@
+a =""">>> a = "Life is too short, You need Python" 
+>>> a[0:4]
+'Life'
+
+>>> a = "20010331Rainy"
+>>> date = a[:8]
+>>> weather = a[8:]
+>>> date
+'20010331'
+>>> weather
+'Rainy'
+"""
+print(a)
+
+
+
+
+
+b = "Life is too short, You need Python"
+print(b[0:4])
+c = "20010331Rainy"
+date = c[:8]
+weather = c[8:]
+print(date)
+print (weather)
+
+
+
+
