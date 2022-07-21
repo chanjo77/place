16813983df52586c8326578eb1fbda6643c4f497
chanjo77 <jocksgml1537@gmail.com>
2022-07-21 20:00:24 +0900

    ex

---
+++ list.py
@@ -0,0 +1,3 @@
+a = [1, 5 ,3, 7 ,2, 9]
+a.insert(0,"andrew")
+print(a)
---
+++ 연습문제1.py
@@ -0,0 +1,35 @@
+#1
+a = [80,75,55]
+b = sum(a)/3
+print(b)
+
+#2
+#c = 13%2
+# c 값이 1 이면 홀수
+# c 값이 0 이면 짝수
+
+#3
+c = "881120-1068234"
+print(c[0:6],c[7:14])
+
+#4 
+pin = "881120-1068234"
+print(pin[7])
+
+#5
+d = "a:b:c:d"
+print(d.replace("a:b:c:d" ,"a#b#c#d"))
+
+#6
+e = [1,3,5,4,2]
+e.sort()
+e.reverse()
+print(e)
+
+
+#7
+f = " ".join(['Life' , 'is' , 'too' , 'short'])
+print(f)
+
+
+
